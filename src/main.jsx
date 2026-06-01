5850cfbfdcc30b91c0bf41aa340a02280e542ee9
Becauseiloveyo <1079458933@qq.com>
2026-06-01 23:38:07 +0800

    Enable smart gameplay optimizer

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './enhancements.css'
+import './gameplayOptimizer.css'
 import './runtimeEnhancements.js'
+import './gameplayOptimizer.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
